e0eb37dcff9953c1d2e8b41b55452979f71fc666
ThanhDev <vuphamngocthanh@gmail.com>
2022-08-24 14:49:24 +0700

    update

--- applicatione.js
+++ applicatione.js
@@ -1,2 +1,18 @@
-let Mobile = function(methods,){
-   this.methodsMobile = methods;
+let Mobile = function(){
+ 
+   let Methodes = function(read,sent,sent){
+      this.setReadMes = function(){
+
+      }
+      this.getReadMes = function(){
+      
+      }
+      this.setSentMes = function(){
+      
+      }
+      this.getSentMes = function(){
+      
+      }
+      this.
+   }
+}
